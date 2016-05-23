6ecdc65a9e78c308298962ac99b027c0926d8ce2
yangshun1029 <yangshun1029@gmail.com>
2016-05-23 08:48:59 +0800

    动态获取aria2gui的路径
    1.动态获取aria2gui的路径
    2.aria2c→aria2gui，避免和其他aria2c应用冲突
    3.动态生成startaria.sh，并且做了判定

--- MacGap/AppDelegate.m
+++ MacGap/AppDelegate.m
@@ -43,17 +43,36 @@
 
 -(void)startAria2
 {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"startaria" ofType:@"sh"];
-    NSTask *task = [[NSTask alloc] init];
-    task.launchPath = @"/bin/sh";
-    task.arguments = @[path];
-    [task launch];
-    //NSArray *arguments = [NSArray arrayWithObjects: @"--enable-rpc=true", @"--check-integrity=true",@"--rpc-listen-all=true", @"--rpc-allow-origin-all", @"--max-connection-per-server=10",@"--max-concurrent-downloads=10",@"--min-split-size=10M",@"--split=10",@"--bt-enable-lpd=true",@"--bt-max-peers=55",@"--bt-require-crypto=true",@"-c",@"-D", @"-d",savePath, nil];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *supportPath=[NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *path = [NSString stringWithFormat:@"%@/%@/sh/",supportPath,[[NSBundle mainBundle] bundleIdentifier]];
+    [fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:NULL];
+    NSString *startAriaPath = [path stringByAppendingPathComponent:@"startaria.sh"];
+    ///Users/Nick/Library/Application Support/com.Aria2GUI/sh/
+    if ([fileManager fileExistsAtPath:startAriaPath])
+    {
+        NSLog(@"文件存在直接运行aria2c");
+        NSTask *task = [[NSTask alloc] init];
+        task.launchPath = @"/bin/sh";
+        task.arguments = @[startAriaPath];
+        [task launch];
+    }
+    else
+    {
+        NSLog(@"文件不存在先创建sh文件，写入配置，再运行aria2c");
+        [fileManager createFileAtPath:startAriaPath contents:nil attributes:nil];
+        NSString *shCommand = [NSString stringWithFormat:@"touch \"/Users/Shared/aria2.session\"\n %@ --input-file=\"/Users/Shared/aria2.session\" --save-session=\"/Users/Shared/aria2.session\" --save-session-interval=10 --dir=\"$HOME/Downloads/\" --force-save=true --max-connection-per-server=10 --max-concurrent-downloads=10 --continue=true --split=10 --force-save=true --min-split-size=10M --enable-rpc=true --rpc-listen-all=false --rpc-listen-port=6800 --rpc-allow-origin-all --check-integrity=true --bt-enable-lpd=true --follow-torrent=true --user-agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0 Safari/601.3.9\" -c -D",[[NSBundle mainBundle] pathForResource:@"aria2gui" ofType:nil]];
+        [shCommand writeToFile:startAriaPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        NSTask *task = [[NSTask alloc] init];
+        task.launchPath = @"/bin/sh";
+        task.arguments = @[startAriaPath];
+        [task launch];
+    }
 }
 
 -(void)closeAria2
 {
-    NSArray *arg =[NSArray arrayWithObjects:@"aria2c",nil];
+    NSArray *arg =[NSArray arrayWithObjects:@"aria2gui",nil];
     NSTask *task=[[NSTask alloc] init];
     task.launchPath = @"/usr/bin/killall";
     task.arguments = arg;
